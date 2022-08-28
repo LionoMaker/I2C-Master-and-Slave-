d8529665f69ca4a562c399eaf9031ec3c2560dc4
Liono Maker <hafizarslan339@gmail.com>
2022-08-28 16:08:58 +0500

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 
 
-// Wire Master Writer
+// Wire Slave Receiver
 // by Nicholas Zambetti <http://www.zambetti.com>
 
 // Demonstrates use of the Wire library
-// Writes data to an I2C/TWI slave device
-// Refer to the "Wire Slave Receiver" example for use with this
+// Receives data as an I2C/TWI slave device
+// Refer to the "Wire Master Writer" example for use with this
 
 // Created 29 March 2006
 
@@ -15,17 +15,22 @@
 #include <Wire.h>
 
 void setup() {
-  Wire.begin(); // join i2c bus (address optional for master)
+  Wire.begin(8);                // join i2c bus with address #8
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           // start serial for output
 }
 
-byte x = 0;
-
 void loop() {
-  Wire.beginTransmission(8); // transmit to device #8
-  Wire.write("x is ");        // sends five bytes
-  Wire.write(x);              // sends one byte
-  Wire.endTransmission();    // stop transmitting
+  delay(100);
+}
 
-  x++;
-  delay(500);
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany) {
+  while (1 < Wire.available()) { // loop through all but the last
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
+  }
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
 }
